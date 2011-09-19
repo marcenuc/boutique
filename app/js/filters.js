d589ea4b7d16e68e1f4311c26466d2c8140693b3
Marcello Nuccio <marcello.nuccio@gmail.com>
2011-09-19 15:32:30 +0200

    Form azienda.

--- app/js/filters.js
+++ app/js/filters.js
@@ -1,1 +1,7 @@
-/* http://docs.angularjs.org/#!angular.filter */
+/*global angular: false */
+
+angular.filter('linkById', function (input) {
+  'use strict';
+  var ids = input.match(/^([a-z]+)_([0-9_]+)$/);
+  return angular.element('<a href="#/' + ids[1] + '/' + ids[2] + '">' + ids[2] + '</a>');
+});
--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -1,1 +1,16 @@
-/* jasmine specs for filters go here */
+/*global describe: false, beforeEach: false, it: false, expect: false, angular: false,
+  binding: false */
+
+describe('Filters', function () {
+  'use strict';
+  
+  describe('linkById', function () {
+    var linkById = angular.filter.linkById;
+    
+    it('should create a link to the document with the given id', function () {
+      var link = linkById('azienda_010101');
+      expect(link.attr('href')).toBe('#/azienda/010101');
+      expect(link.text()).toBe('010101');
+    });
+  });
+});
