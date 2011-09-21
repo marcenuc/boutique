32fabf5d4175b7732aed2d3b231b8ec3b2cc6c44
Marcello Nuccio <marcello.nuccio@gmail.com>
2011-09-21 21:23:00 +0200

    Fixed redirection on create/update of azienda.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,48 +1,77 @@
 /*global angular: false */
 
-function AziendaCtrl($routeParams, Document, Validator) {
+function AziendaCtrl($routeParams, Document, Validator, userCtx, $location) {
   'use strict';
   var self = this;
   this.Document = Document;
   this.Validator = Validator;
-  
+  this.userCtx = userCtx;
+  this.$location = $location;
+
   this.aziende = Document.aziende(function () {
     if ($routeParams.codice) {
       self.selectCodice($routeParams.codice);
     }
   });
-  this.azienda = { _id: $routeParams.codice };
-  this.flash = {};
+  this.azienda = { _id: Document.toAziendaId($routeParams.codice) };
+
+  this.flash = userCtx.flash;
+  userCtx.flash = {};
 }
-AziendaCtrl.$inject = ['$routeParams', 'Document', 'Validator'];
+AziendaCtrl.$inject = ['$routeParams', 'Document', 'Validator', 'userCtx', '$location'];
 
 (function () {
   'use strict';
-  
+
   AziendaCtrl.prototype = {
-      
+
+    getAziendaAtIdx: function () {
+      if (angular.isDefined(this.aziendaIdx)) {
+        return this.aziende.rows[this.aziendaIdx].doc;
+      }
+    },
+
+    isIdChanged: function () {
+      var selectedAzienda = this.getAziendaAtIdx();
+      return selectedAzienda && selectedAzienda._id !== this.azienda._id;
+    },
+
     validate: function () {
-      this.flash = this.Validator.check(this.azienda);
+      this.flash = this.Validator.check(this.azienda, this.getAziendaAtIdx());
       return this.flash.errors.length === 0;
     },
-    
+
     save: function () {
       var self = this;
+      if (this.isIdChanged()) {
+        delete this.azienda._rev;
+        delete this.aziendaIdx;
+      }
       if (this.validate()) {
         this.Document.save(this.azienda, function (res) {
+          var isNew = !self.azienda._rev,
+            notice = { notice: [{ message: 'Salvato' }] };
           self.azienda._rev = res.rev;
-          self.flash.notice = [{ message: 'Salvato' }];
+          if (isNew) {
+            self.userCtx.flash = notice;
+            self.aziende.rows.push({ doc: angular.copy(self.azienda) });
+            self.$location.path('/azienda/' + self.Document.toCodice(self.azienda._id)).replace();
+          } else {
+            self.flash = notice;
+            angular.copy(self.azienda, self.aziende.rows[self.aziendaIdx].doc);
+          }
         });
       }
     },
-    
+
     select: function (idx) {
       this.azienda = angular.copy(this.aziende.rows[idx].doc);
+      this.aziendaIdx = idx;
     },
-    
+
     selectCodice: function (codice) {
       var self = this,
-        id = 'azienda_' + codice;
+        id = this.Document.toAziendaId(codice);
       return this.aziende.rows.some(function (row, idx) {
         if (row.id === id) {
           self.select(idx);
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -3,7 +3,7 @@
 
 describe('Controllers', function () {
   'use strict';
-  
+
   var aziende = {
     "total_rows" : 15,
     "offset" : 1,
@@ -39,88 +39,214 @@ describe('Controllers', function () {
       }
     } ]
   };
-      
+
   describe('AziendaCtrl', function () {
-    var scope, $browser = null, $routeParams = null, ctrl = null;
+    var scope = null, $browser = null, $routeParams = null, ctrl = null;
+
+    function newController() {
+      $browser.xhr.expectGET('/boutique_db/_all_docs?endkey=%22azienda_%EF%BF%B0%22&include_docs=true&startkey=%22azienda_%22').respond(angular.copy(aziende));
+      ctrl = scope.$new(AziendaCtrl);
+    }
 
     beforeEach(function () {
       scope = angular.scope();
       $browser = scope.$service('$browser');
       $routeParams = scope.$service('$routeParams');
-
-      $browser.xhr.expectGET('/boutique_db/_all_docs?endkey=%22azienda_%EF%BF%B0%22&include_docs=true&startkey=%22azienda_%22').respond(aziende);
-      ctrl = scope.$new(AziendaCtrl);
+      ctrl = null;
     });
-    
+
     afterEach(function () {
       expect($browser.xhr.requests.length).toBe(0, 'You have not flushed the $browser.xhr requests.');
     });
-    
-    
+
+
     describe('inizialization', function () {
+
+      it('should set aziendaIdx to undefined', function () {
+        newController();
+        $browser.xhr.flush();
+        expect(ctrl.aziendaIdx).toBeUndefined();
+      });
+
       it('should have an empty list of aziende before fetching', function () {
+        newController();
         expect(ctrl.aziende).toEqualData({});
         $browser.xhr.flush();
       });
 
-      it('should populate "aziende" model with all aziende fetched from xhr', function () {
+      it('should fetch all aziende', function () {
+        newController();
         $browser.xhr.flush();
         expect(ctrl.aziende).toEqualData(aziende);
       });
-      
-      it('should set azienda._id to $routeParams.codice after fetching aziende', function () {
-        $routeParams.codice = '099997';        
+
+      it('should set azienda and aziendaIdx according to $routeParams.codice', function () {
+        var azienda = aziende.rows[1].doc,
+          Document = scope.$service('Document');
+        $routeParams.codice = Document.toCodice(azienda._id);
+        newController();
+        expect(ctrl.azienda._id).toBe(azienda._id);
         $browser.xhr.flush();
-        expect(ctrl.azienda).toEqual(aziende.rows[1].doc);
+        expect(ctrl.azienda).toEqual(azienda);
+        expect(ctrl.aziendaIdx).toBe(1);
+      });
+
+      it('should set flash to userCtx.flash and reset the latter', function () {
+        var userCtx = scope.$service('userCtx'),
+          flash = { errors: [{ message: 'sample error' }] };
+        userCtx.flash = flash;
+        newController();
+        $browser.xhr.flush();
+        expect(ctrl.flash).toEqual(flash);
+        expect(userCtx.flash).toEqual({});
       });
     });
-    
+
 
     describe('actions', function () {
       beforeEach(function () {
+        newController();
         $browser.xhr.flush();
       });
-      
+
+      describe('validate', function () {
+        it('should set flash to validation errors', function () {
+          expect(ctrl.flash).toBeUndefined();
+          ctrl.azienda = { _id: 'azienda_000001' };
+          expect(ctrl.validate()).toBe(false);
+          expect(ctrl.flash).toEqual({ errors: [{ message: 'Required: nome' }] });
+          ctrl.azienda = { _id: 'azienda_000001', nome: 'a' };
+          expect(ctrl.validate()).toBe(true);
+          expect(ctrl.flash).toEqual({ errors: [] });
+        });
+
+
+      });
+
       describe('save', function () {
+        var response = { ok: true, rev: '1' };
+
+        function doSave() {
+          $browser.xhr.expectPUT('/boutique_db/' + ctrl.azienda._id, ctrl.azienda).respond(response);
+          ctrl.save();
+          $browser.xhr.flush();
+        }
+
+        function expectAppendedAzienda() {
+          var newCount = ctrl.aziende.rows.length,
+            oldCount = aziende.rows.length;
+          expect(newCount).toBe(oldCount + 1);
+          expect(ctrl.aziende.rows[newCount - 1].doc).toEqualData(ctrl.azienda);
+        }
+
+
         it('should not submit the data if not valid', function () {
-          ctrl.azienda = { _id: 'azienda_010101', _rev: '1', nome: ''};
+          ctrl.azienda = { _id: 'azienda_010101', _rev: '1', nome: '' };
           ctrl.save();
           expect(ctrl.azienda._rev).toBe('1');
           expect(ctrl.flash.errors[0]).toEqual({ message: 'Required: nome' });
         });
-      
-        it('should set _rev field in new documents', function () {
-          ctrl.azienda = { _id: 'azienda_010101', nome: 'Nuova azienda' };
-          $browser.xhr.expectPUT('/boutique_db/azienda_010101', ctrl.azienda).respond({ ok: true, rev: '1' });
+
+        it('should create new document if id changed', function () {
+          ctrl.select(0);
+          ctrl.azienda._id = 'azienda_000000';
+          var az = angular.copy(ctrl.azienda);
+          delete az._rev;
+          $browser.xhr.expectPUT('/boutique_db/azienda_000000', az).respond(response);
           ctrl.save();
           $browser.xhr.flush();
-          expect(ctrl.azienda._rev).toBe('1');
+          expectAppendedAzienda();
         });
-      
-        it('should update _rev field in existing documents', function () {
-          ctrl.azienda = { _id: 'azienda_010101', _rev: '1', nome: 'Vecchia azienda' };
-          $browser.xhr.expectPUT('/boutique_db/azienda_010101', ctrl.azienda).respond({ ok: true, rev: '2' });
-          ctrl.save();
-          $browser.xhr.flush();
-          expect(ctrl.azienda._rev).toBe('2');
+
+
+        describe('new document', function () {
+
+          beforeEach(function () {
+            ctrl.azienda = { _id: 'azienda_010101', nome: 'Nuova azienda' };
+            doSave();
+          });
+
+          it('should set _rev field to the returned revision', function () {
+            expect(ctrl.azienda._rev).toBe(response.rev);
+          });
+
+          it('should append azienda to aziende', function () {
+            expectAppendedAzienda();
+          });
+
+          it('should redirect to azienda page', function () {
+            expect(scope.$service('$location').path()).toBe('/azienda/010101');
+          });
+
+        });
+
+        describe('existing document', function () {
+
+          beforeEach(function () {
+            ctrl.select(0);
+            doSave();
+          });
+
+          it('should update _rev field to the returned revision', function () {
+            expect(ctrl.azienda._rev).toBe(response.rev);
+          });
+
+          it('should update aziende when saving updates to azienda', function () {
+            expect(ctrl.azienda).toEqualData(ctrl.aziende.rows[0].doc);
+          });
         });
       });
-    
+
+
       describe('select', function () {
+
         it('should copy to azienda from aziende at given index', function () {
           ctrl.select(1);
-          expect(ctrl.azienda).toEqualData(aziende.rows[1].doc);
+          expect(ctrl.azienda).toEqualData(ctrl.aziende.rows[1].doc);
           expect(ctrl.azienda === ctrl.aziende.rows[1].doc).toBe(false);
-        });      
+        });
+
+        it('should cache selected index in aziendaIdx', function () {
+          ctrl.select(0);
+          expect(ctrl.aziendaIdx).toBe(0);
+          ctrl.select(1);
+          expect(ctrl.aziendaIdx).toBe(1);
+        });
       });
-    
+
+
       describe('selectCodice', function () {
+
         it('should copy to azienda from aziende with given codice', function () {
           expect(ctrl.selectCodice('000000')).toBe(false);
           expect(ctrl.azienda).toEqualData({});
           expect(ctrl.selectCodice('099997')).toBe(true);
           expect(ctrl.azienda).toEqualData(aziende.rows[1].doc);
           expect(ctrl.azienda === ctrl.aziende.rows[1].doc).toBe(false);
+        });
+      });
+
+
+      describe('getAziendaAtIdx', function () {
+
+        it('should return undefined if aziendaIdx is undefined', function () {
+          ctrl.aziendaIdx = undefined;
+          expect(ctrl.getAziendaAtIdx()).toBeUndefined();
+        });
+
+        it('should return the first record in aziende if aziendaIdx is 0', function () {
+          ctrl.aziendaIdx = 0;
+          expect(ctrl.getAziendaAtIdx()).toBe(ctrl.aziende.rows[0].doc);
+        });
+      });
+
+
+      describe('isIdChanged', function () {
+        it('should return true only if _id of azienda is not equal to _id of azienda at aziendaIdx', function () {
+          ctrl.select(0);
+          expect(ctrl.isIdChanged()).toBe(false);
+          ctrl.azienda._id = 'azienda_999999';
+          expect(ctrl.isIdChanged()).toBe(true);
         });
       });
     });
