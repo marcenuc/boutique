4c8a1f404d96f1ee248878e2b8c1957eaf90afab
Marcello Nuccio <marcello.nuccio@gmail.com>
2011-10-03 06:32:10 +0200

    Fix type in bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ git submodule init
 git submodule update
 ln -s ../lib/connect-exec node_modules/
 
-echo 'Maybe you need to "cd lib/As400Querier && mvn package" to start."
+echo 'Maybe you need to "cd lib/As400Querier && mvn package" to start.'
