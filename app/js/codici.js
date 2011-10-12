070e4e2a5172942c4d94456e948f97fe0985cbba
Marcello Nuccio <marcello.nuccio@gmail.com>
2011-10-12 10:05:51 +0200

    new helper CODICI.parseMoney().

--- app/js/codici.js
+++ app/js/codici.js
@@ -16,25 +16,26 @@ var CODICI;
     return (/^\d+$/).test(code);
   };
 
-  CODICI.padZero = function (code, len) {
+  function padZero(code, len) {
     return CODICI.isCode(code) ? new Array(len + 1 - code.length).join('0') + code : null;
-  };
+  }
+  CODICI.padZero = padZero;
 
   CODICI.codiceListino = function (stagione, modello, articolo) {
-    var s = CODICI.padZero(stagione, CODICI.LEN_STAGIONE),
-      m = CODICI.padZero(modello, CODICI.LEN_MODELLO),
-      a = CODICI.padZero(articolo, CODICI.LEN_ARTICOLO);
+    var s = padZero(stagione, CODICI.LEN_STAGIONE),
+      m = padZero(modello, CODICI.LEN_MODELLO),
+      a = padZero(articolo, CODICI.LEN_ARTICOLO);
     if (s && m && a) {
       return s + m + a;
     }
   };
 
   CODICI.codiceAs400 = function (stagione, modello, articolo, colore, taglia) {
-    var s = CODICI.padZero(stagione, CODICI.LEN_STAGIONE),
-      m = CODICI.padZero(modello, CODICI.LEN_MODELLO),
-      a = CODICI.padZero(articolo, CODICI.LEN_ARTICOLO),
-      c = CODICI.padZero(colore, CODICI.LEN_COLORE),
-      t = CODICI.padZero(taglia, CODICI.LEN_TAGLIA);
+    var s = padZero(stagione, CODICI.LEN_STAGIONE),
+      m = padZero(modello, CODICI.LEN_MODELLO),
+      a = padZero(articolo, CODICI.LEN_ARTICOLO),
+      c = padZero(colore, CODICI.LEN_COLORE),
+      t = padZero(taglia, CODICI.LEN_TAGLIA);
     if (s && m && a && c && t) {
       return s + m + a + c + t;
     }
@@ -90,6 +91,19 @@ var CODICI;
       return ['Descrizioni taglia (' + descrizioneTaglia + ') non trovate per scalarino ' + scalarino + ': stagione="' + stagione + '", modello="' + modello + '"'];
     }
     return ['Modello non in anagrafe: stagione="' + stagione + '", modello="' + modello + '"'];
+  };
+
+
+  function padCents(n) {
+    return n ? (n.length < 2 ? n + '0' : n) : '00';
+  }
+
+  CODICI.parseMoney = function (value) {
+    var mp = /^([0-9]+)(?:\.([0-9]{1,2}))?$/.exec(value);
+    if (mp) {
+      return [null, parseInt(mp[1] + padCents(mp[2]), 10)];
+    }
+    return ['Invalid amount for money: ' + value];
   };
 }());
 
---
+++ test-srv/unit/codici.spec.js
@@ -0,0 +1,31 @@
+/*jslint node: true */
+/*jshint node: true */
+/*global describe: false, beforeEach: false, afterEach: false, it: false, xit: false, expect: false, jasmine: false */
+
+var requirejs = require('requirejs');
+requirejs.config({
+  baseUrl: __dirname + '/../..',
+  nodeRequire: require
+});
+
+requirejs(['app/js/codici'], function (codici) {
+  'use strict';
+
+  describe('parseMoney', function () {
+    it('should add cents to integer values', function () {
+      expect(codici.parseMoney('12')).toEqual([null, 1200]);
+    });
+
+    it('should add cents to float values with one digit after dot', function () {
+      expect(codici.parseMoney('12.3')).toEqual([null, 1230]);
+    });
+
+    it('should convert to cents integer value', function () {
+      expect(codici.parseMoney('12.34')).toEqual([null, 1234]);
+    });
+
+    it('should return error for more than two digits after dot', function () {
+      expect(codici.parseMoney('12.345')).toEqual(['Invalid amount for money: 12.345']);
+    });
+  });
+});
