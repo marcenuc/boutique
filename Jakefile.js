b090ac8fe38ed0e0ee4ad40f61644a6ee65cb2fa
Marcello Nuccio <marcello.nuccio@gmail.com>
2011-10-21 18:02:11 +0200

    Task stampaEtichetteFromBollaAs400.

--- Jakefile.js
+++ Jakefile.js
@@ -432,6 +432,22 @@ requirejs(['require', 'lib/taskutil', 'util', 'path', 'cradle', 'lib/servers'], 
     });
   });
 
+  desc('Produce un file di testo con la stampa delle etichette da una bolla as400');
+  task('stampaEtichetteFromBollaAs400', function (idBollaAs400, comparator) {
+    requirejs(['lib/etichette'], function (etichette) {
+      var db = newBoutiqueDbConnection();
+      etichette.stampaFromBollaAs400(idBollaAs400, db, comparator, function (err, warns, stampa) {
+        if (err) {
+          return fail(util.inspect(err));
+        }
+        if (warns && warns.length) {
+          console.warn(warns.join('\n'));
+        }
+        process.stdout.write(stampa);
+      });
+    });
+  });
+
   desc('Produce un file di testo con la stampa delle etichette');
   task('stampaEtichette', function (azienda, comparator) {
     requirejs(['lib/etichette'], function (etichette) {
