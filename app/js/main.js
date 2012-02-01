2ec76caf011f0adf23b11eac19cd0cc87dbcac07
Marcello Nuccio <marcello.nuccio@gmail.com>
2012-02-01 22:14:56 +0100

    chore(build) we don't use RequireJS anymore.

--- app/js/main.js
+++
@@ -1,8 +0,0 @@
-/*global require:false, angular:false, document:false*/
-
-require(['jszip', 'angular', 'config', 'codici', 'validators', 'app', 'services', 'controllers', 'filters', 'widgets'], function () {
-  'use strict';
-  angular.element(document).ready(function () {
-    angular.bootstrap(document, ['app']);
-  });
-});
