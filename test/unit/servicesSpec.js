abdb41e5e35894a10b4c2f98c4da637096c9227a
Marcello Nuccio <marcello.nuccio@gmail.com>
2012-02-10 10:33:22 +0100

    chore(test) don't inject() top-level beforeEach()
    It makes impossible to use module() in tests.

--- test/unit/servicesSpec.js
+++ test/unit/servicesSpec.js
@@ -1,104 +1,98 @@
-/*global describe:false, beforeEach:false, afterEach:false, it:false, expect:false, module:false, inject:false*/
+/*global describe:false, beforeEach:false, afterEach:false, it:false, expect:false, module:false, inject:false, jasmine:false*/
 describe('Service', function () {
   'use strict';
   beforeEach(module('app.services', 'app.shared', function ($provide) {
     $provide.value('couchdb', { db: 'db', designDoc: 'ddoc' });
   }));
 
-  var $hb;
-
-  beforeEach(inject(function ($httpBackend) {
-    $hb = $httpBackend;
+  afterEach(inject(function ($httpBackend) {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
   }));
 
-  afterEach(function () {
-    $hb.verifyNoOutstandingExpectation();
-    $hb.verifyNoOutstandingRequest();
-  });
-
   describe('CdbView', function () {
-    it('should query view ultimoNumero', inject(function (CdbView) {
-      $hb.expectGET('/db/_design/ddoc/_view/contatori?descending=true&endkey=%5B%22010101%22,2012,%22D%22%5D&limit=1&startkey=%5B%22010101%22,2012,%22D%22,%7B%7D%5D').respond({});
+    it('should query view ultimoNumero', inject(function ($httpBackend, CdbView) {
+      $httpBackend.expectGET('/db/_design/ddoc/_view/contatori?descending=true&endkey=%5B%22010101%22,2012,%22D%22%5D&limit=1&startkey=%5B%22010101%22,2012,%22D%22,%7B%7D%5D').respond({});
       CdbView.ultimoNumero('010101', '2012', 'D');
-      $hb.flush();
+      $httpBackend.flush();
     }));
   });
 
   describe('Document', function () {
     describe('get', function () {
-      it('should query document by id', inject(function (Document) {
+      it('should query document by id', inject(function ($httpBackend, Document) {
         var scalarini, resp = { _id: 'Scalarini', data: 'abcd' };
-        $hb.expectGET('/db/Scalarini').respond(JSON.stringify(resp));
+        $httpBackend.expectGET('/db/Scalarini').respond(JSON.stringify(resp));
         scalarini = Document.get({ id: resp._id });
         expect(scalarini).toEqualData({});
-        $hb.flush();
+        $httpBackend.flush();
         expect(scalarini).toEqualData(resp);
       }));
     });
 
     describe('clienti', function () {
-      it('should query all docs of type Cliente of the given azienda', inject(function (Document) {
-        $hb.expectGET('/db/_all_docs?endkey=%22Cliente_010101_%EF%BF%B0%22&include_docs=true&startkey=%22Cliente_010101_%22').respond({ rows: [] });
+      it('should query all docs of type Cliente of the given azienda', inject(function ($httpBackend, Document) {
+        $httpBackend.expectGET('/db/_all_docs?endkey=%22Cliente_010101_%EF%BF%B0%22&include_docs=true&startkey=%22Cliente_010101_%22').respond({ rows: [] });
         var query = Document.clienti('Azienda_010101');
         expect(query).toEqualData({});
-        $hb.flush();
+        $httpBackend.flush();
         expect(query).toEqualData({ rows: [] });
       }));
     });
 
     describe('save', function () {
-      it('should PUT new documents', inject(function (Document) {
+      it('should PUT new documents', inject(function ($httpBackend, Document) {
         var data = { nome: 'Azienda 010101' },
           azienda = new Document(data),
           response = { ok: true, id: 'Azienda_010101', rev: '946B7D1C' };
 
-        $hb.expectPUT('/db/Azienda_010101', data).respond(JSON.stringify(response));
+        $httpBackend.expectPUT('/db/Azienda_010101', data).respond(JSON.stringify(response));
         azienda.$save({ id: 'Azienda_010101' }, function (resp) {
           expect(resp).toEqualData(response);
         });
-        $hb.flush();
+        $httpBackend.flush();
       }));
 
-      it('should PUT updated documents', inject(function (Document) {
+      it('should PUT updated documents', inject(function ($httpBackend, Document) {
         var data = { _id: 'Azienda_010101', nome: 'Azienda 010101' },
           azienda = new Document(data),
           response = { ok: true, id: 'Azienda_010101', rev: '946B7D1C' };
-        $hb.expectPUT('/db/Azienda_010101', data).respond(JSON.stringify(response));
+        $httpBackend.expectPUT('/db/Azienda_010101', data).respond(JSON.stringify(response));
         azienda.$save(function (resp) {
           expect(resp).toEqualData(response);
         });
-        $hb.flush();
+        $httpBackend.flush();
       }));
     });
   });
 
   describe('as400', function () {
     describe('bolla', function () {
-      it('should query with the given parameters', inject(function (As400) {
+      it('should query with the given parameters', inject(function ($httpBackend, As400) {
         var intestazioneBolla = {
             data: '110704',
             numero: '40241',
             enteNumerazione: 'Y',
             codiceNumerazione: '10'
           };
-        $hb.expectGET('../as400/bolla/data=110704/numero=40241/enteNumerazione=Y/codiceNumerazione=10').respond(JSON.stringify(intestazioneBolla));
+        $httpBackend.expectGET('../as400/bolla/data=110704/numero=40241/enteNumerazione=Y/codiceNumerazione=10').respond(JSON.stringify(intestazioneBolla));
         As400.bolla(intestazioneBolla, function (data) {
           expect(data).toEqual(intestazioneBolla);
         });
-        $hb.flush();
+        $httpBackend.flush();
       }));
     });
   });
 
   describe('session', function () {
-    it('should query session data', function () {
+    it('should query session data', inject(function ($httpBackend) {
       var session = { userCtx: { name: 'boutique' } };
-      $hb.expectGET('../_session').respond(session);
+      $httpBackend.expectGET('../_session').respond(session);
       inject(['session', function (session) {
-        $hb.flush();
+        $httpBackend.flush();
         expect(session).toEqual(session);
       }]);
-    });
+    }));
   });
 
   describe('Azienda', function () {
@@ -107,19 +101,20 @@ describe('Service', function () {
       { id: 'Azienda_010101', key: '010101', doc: { _id: 'Azienda_010101', nome: 'Neg1', tipo: 'NEGOZIO' } },
       { id: 'Azienda_020202', key: '020202', doc: { _id: 'Azienda_020202', nome: 'Neg2', tipo: 'NEGOZIO' } }
     ] };
-    describe('all', function () {
-      beforeEach(function () {
-        $hb.expectGET('/db/_design/ddoc/_view/aziende?include_docs=true').respond(JSON.stringify(aziende));
-      });
 
-      it('should return map of all aziende', inject(function (Azienda) {
+    beforeEach(inject(function ($httpBackend) {
+      $httpBackend.expectGET('/db/_design/ddoc/_view/aziende?include_docs=true').respond(JSON.stringify(aziende));
+    }));
+
+    describe('all', function () {
+      it('should return map of all aziende', inject(function ($httpBackend, Azienda) {
         var resp = Azienda.all();
         expect(typeof resp.then).toBe('function');
         resp.success(function (data) {
           expect(typeof data).toBe('object');
           expect(Object.keys(data)).toEqual(['099999', '010101', '020202']);
         });
-        $hb.flush();
+        $httpBackend.flush();
       }));
     });
   });
