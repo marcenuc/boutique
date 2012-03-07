8c1d3b5160356df36466340270157b3db96e7882
Marcello Nuccio <marcello.nuccio@gmail.com>
2012-03-07 10:49:46 +0100

    feat(giacenzeAnno) add total.

--- lists.js
+++ lists.js
@@ -48,18 +48,21 @@ define(function () {
     },
 
     giacenzeTSV: function listGiacenzeTSV() {
-      var row;
+      var row, k, i = 1;
       start({ headers: { 'Content-Type': 'application/excel' } });
       row = getRow();
       if (row) {
-        send('ANNO\tMODELLO\tARTICOLO\tCOLORE\tSTAGIONE\tMAGAZZINO\tIN_PRODUZIONE\tTIPO_MAGAZZINO\tSCALARINO\tTAGLIA\tDESCRIZIONE_TAGLIA\tDESCRIZIONE\tCOSTO\tGIACENZA\r\n');
+        send('ANNO\tMODELLO\tARTICOLO\tCOLORE\tSTAGIONE\tMAGAZZINO\tIN_PRODUZIONE\tTIPO_MAGAZZINO\tSCALARINO\tTAGLIA\tDESCRIZIONE_TAGLIA\tDESCRIZIONE\tCOSTO\tGIACENZA\tTOTALE\r\n');
       }
       while (row) {
         if (row.value !== 0) {
-          send([row.key.join('\t'), '\t', row.value, '\r\n'].join(''));
+          k = row.key;
+          i += 1;
+          send(['"', k.slice(0, 7).join('"\t"'), '"\t', k[7], '\t', k[8], '\t"', k.slice(9, 12).join('"\t"'), '"\t', k[12], '\t', row.value, '\t=M', i, '*N', i, '/100\r\n'].join(''));
         }
         row = getRow();
       }
+      send('""\t""\t""\t""\t""\t""\t""\t\t\t""\t""\t""\t\t\t=SOMMA(O2:O' + i + ')\r\n');
     }
   };
 });
--- test-srv/couchdb/lists.spec.js
+++ test-srv/couchdb/lists.spec.js
@@ -8,7 +8,7 @@ requirejs(['lists'], function (lists) {
   var cols;
 
   beforeEach(function () {
-    cols = 'ANNO\tMODELLO\tARTICOLO\tCOLORE\tSTAGIONE\tMAGAZZINO\tIN_PRODUZIONE\tTIPO_MAGAZZINO\tSCALARINO\tTAGLIA\tDESCRIZIONE_TAGLIA\tDESCRIZIONE\tCOSTO\tGIACENZA';
+    cols = 'ANNO\tMODELLO\tARTICOLO\tCOLORE\tSTAGIONE\tMAGAZZINO\tIN_PRODUZIONE\tTIPO_MAGAZZINO\tSCALARINO\tTAGLIA\tDESCRIZIONE_TAGLIA\tDESCRIZIONE\tCOSTO\tGIACENZA\tTOTALE';
     lists._reset();
   });
 
@@ -17,33 +17,35 @@ requirejs(['lists'], function (lists) {
       start = { headers: { 'Content-Type': 'application/excel' } };
     it('should send TSV output', function () {
       var rows = [
-        { key: ['2011', '10102', '1444', '7300', '922', '099994', false, 3, 3, '01', 'TU', 'CRAVATTA CM 8,5', 100], value: 1 },
-        { key: ['2011', '10102', '1699', '6100', '132', '019996', false, 2, 3, '01', 'TU', 'CRAVATTA CM 8,5', 200], value: 1 },
-        { key: ['2011', '10102', '1699', '6100', '132', '019998', false, 2, 3, '01', 'TU', 'CRAVATTA CM 8,5', 300], value: 1 }
+        { key: ['2011', '10102', '1444', '7300', '922', '099994', false, 3, 3, '01', 'TU', 'CRAVATTA CM 8,5', 123], value: 1 },
+        { key: ['2011', '10102', '1699', '6100', '132', '019996', false, 2, 3, '01', 'TU', 'CRAVATTA CM 8,5', 234], value: 1 },
+        { key: ['2011', '10102', '1699', '6100', '132', '019998', false, 2, 3, '01', 'TU', 'CRAVATTA CM 8,5', 345], value: 1 }
       ];
       lists._setRows(rows);
       list();
       expect(lists._output()).toEqual([start, [
         cols,
-        '2011\t10102\t1444\t7300\t922\t099994\tfalse\t3\t3\t01\tTU\tCRAVATTA CM 8,5\t100\t1',
-        '2011\t10102\t1699\t6100\t132\t019996\tfalse\t2\t3\t01\tTU\tCRAVATTA CM 8,5\t200\t1',
-        '2011\t10102\t1699\t6100\t132\t019998\tfalse\t2\t3\t01\tTU\tCRAVATTA CM 8,5\t300\t1',
+        '"2011"\t"10102"\t"1444"\t"7300"\t"922"\t"099994"\t"false"\t3\t3\t"01"\t"TU"\t"CRAVATTA CM 8,5"\t123\t1\t=M2*N2/100',
+        '"2011"\t"10102"\t"1699"\t"6100"\t"132"\t"019996"\t"false"\t2\t3\t"01"\t"TU"\t"CRAVATTA CM 8,5"\t234\t1\t=M3*N3/100',
+        '"2011"\t"10102"\t"1699"\t"6100"\t"132"\t"019998"\t"false"\t2\t3\t"01"\t"TU"\t"CRAVATTA CM 8,5"\t345\t1\t=M4*N4/100',
+        '""\t""\t""\t""\t""\t""\t""\t\t\t""\t""\t""\t\t\t=SOMMA(O2:O4)',
         ''
       ].join('\r\n')]);
     });
 
     it('should exclude rows with value=0', function () {
       var rows = [
-        { key: ['2011', '10102', '1444', '7300', '922', '099994', false, 3, 3, '01', 'TU', 'CRAVATTA CM 8,5', 100], value: 1 },
-        { key: ['2011', '10102', '1699', '6100', '132', '019996', false, 2, 3, '01', 'TU', 'CRAVATTA CM 8,5', 200], value: 0 },
-        { key: ['2011', '10102', '1699', '6100', '132', '019998', false, 2, 3, '01', 'TU', 'CRAVATTA CM 8,5', 300], value: 1 }
+        { key: ['2011', '10102', '1444', '7300', '922', '099994', false, 3, 3, '01', 'TU', 'CRAVATTA CM 8,5', 123], value: 1 },
+        { key: ['2011', '10102', '1699', '6100', '132', '019996', false, 2, 3, '01', 'TU', 'CRAVATTA CM 8,5', 234], value: 0 },
+        { key: ['2011', '10102', '1699', '6100', '132', '019998', false, 2, 3, '01', 'TU', 'CRAVATTA CM 8,5', 345], value: 1 }
       ];
       lists._setRows(rows);
       list();
       expect(lists._output()).toEqual([start, [
         cols,
-        '2011\t10102\t1444\t7300\t922\t099994\tfalse\t3\t3\t01\tTU\tCRAVATTA CM 8,5\t100\t1',
-        '2011\t10102\t1699\t6100\t132\t019998\tfalse\t2\t3\t01\tTU\tCRAVATTA CM 8,5\t300\t1',
+        '"2011"\t"10102"\t"1444"\t"7300"\t"922"\t"099994"\t"false"\t3\t3\t"01"\t"TU"\t"CRAVATTA CM 8,5"\t123\t1\t=M2*N2/100',
+        '"2011"\t"10102"\t"1699"\t"6100"\t"132"\t"019998"\t"false"\t2\t3\t"01"\t"TU"\t"CRAVATTA CM 8,5"\t345\t1\t=M3*N3/100',
+        '""\t""\t""\t""\t""\t""\t""\t\t\t""\t""\t""\t\t\t=SOMMA(O2:O3)',
         ''
       ].join('\r\n')]);
     });
