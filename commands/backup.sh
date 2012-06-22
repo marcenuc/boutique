fb404f4e7e95e9044898072dd7d7c89df330052a
Marcello Nuccio <marcello.nuccio@gmail.com>
2012-06-22 17:55:31 +0200

    fix(backup) use home

--- commands/backup.sh
+++ commands/backup.sh
@@ -11,7 +11,7 @@ log () {
 for i in $BACKUP_DATA_FILES; do
   archive_file="$hostname$(echo $i |sed -e 's#[/ ]#_#g')-$day.tar.gz"
   log "Backing up $i to $archive_file: "
-  local_archive_file=/var/backups/$archive_file
+  local_archive_file=$archive_file
   tar czf $local_archive_file $i
   log " - $(ls -lh $local_archive_file)"
   smbclient -A backup-access.cfg -c "cd $BACKUP_SHARE_FOLDER;prompt OFF;put $local_archive_file" $BACKUP_SHARE
